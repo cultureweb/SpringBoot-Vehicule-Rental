2b0c205e214ff03a482c962999d1c7d6188fa421
ced <cedric.milius@yahoo.fr>
2019-11-21 17:29:32 +0100

    add eureka

--- src/main/java/com/vehiclerental/restservice/RestApp/RestAppApplication.java
+++ src/main/java/com/vehiclerental/restservice/RestApp/RestAppApplication.java
@@ -2,10 +2,14 @@ package com.vehiclerental.restservice.RestApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 @EnableSwagger2
+@EnableEurekaClient
 public class RestAppApplication {
 
 	public static void main(String[] args)
